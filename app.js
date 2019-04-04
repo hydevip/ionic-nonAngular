7af548b13cabfae8fa155db65edde0906995d143
hydevip <35841222+hydevip@users.noreply.github.com>
2019-04-04 12:34:41 +0200

    input and list

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const amountInput= document.querySelector('#input-amount');
 const cancelBtn= document.querySelector('#btn-cancel');
 const confirmBtn= document.querySelector('#btn-confirm');
 const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
+
+
+let totalExpenses= 0;
+
+const clear=()=>{
+    reasonInput.value='';
+    amountInput.value='';
+};
 
 
 
@@ -14,7 +23,17 @@ confirmBtn.addEventListener('click', ()=>{
         return;
     }
     const newItem= document.createElement('ion-item');
-    newItem.textContent = enteredReason + ':$' + enteredAmount;
+    newItem.textContent = enteredReason + ': $' + enteredAmount;
 
     expensesList.appendChild(newItem);
+
+console.log(typeof enteredAmount);
+
+    totalExpenses += +enteredAmount;  // +enteredAmount it converts the enteredAmount from string to integer
+    totalExpensesOutput.textContent= totalExpenses;
+
+    clear();
+  
 });
+
+cancelBtn.addEventListener('click', clear);
